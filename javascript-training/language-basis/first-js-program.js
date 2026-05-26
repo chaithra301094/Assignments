e21067efc382d0826e8957f923dbb68362fd3d02
Chaithra Rajendran <chaithrahema1@gmail.com>
2026-05-26 10:19:40 +0530

    Assignment 1

---
+++ javascript-training/language-basis/first-js-program.js
@@ -0,0 +1,5 @@
+// console.log is used as print output in javascript.
+console.log("Hello world!")
+console.info("This is an info message.")
+console.warn("This is a warning message.")
+console.error("This is an error message.")
---
+++ typecript-training/language-basics/data-types.ts
@@ -0,0 +1,14 @@
+let ssn :number = 765345221;
+let firstName :string = "John";
+
+interface personInfo {
+    name :string;
+    age :number;
+    address :string;
+}
+
+let person :personInfo = {
+    name : "John",
+    age : 30,
+    address : "123 Main St" 
+};
