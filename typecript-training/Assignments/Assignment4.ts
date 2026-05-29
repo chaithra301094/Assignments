c4460980707aa4bbf7c44dac76bf9b98f9add5c5
Chaithra Rajendran <chaithrahema1@gmail.com>
2026-05-29 06:52:28 +0530

    Assignment 4

---
+++ typecript-training/Assignments/Assignment4.ts
@@ -0,0 +1,19 @@
+let studentsName: string[] = ["Suresh","Mahesh" , "Naresh"];
+let studentsMark: number[] =  [75,80,82];
+
+let sum: number = 10;
+let studentsTotalMarksArray: number [] = [];
+let totalMarks : number = 0;
+for ( let i:number = 0; i<=2; i++)
+{
+  studentsTotalMarksArray[i] =  sum+ studentsMark[i] ;
+   totalMarks = totalMarks + studentsTotalMarksArray[i];
+   
+}
+
+let avarageMarks: number = totalMarks/studentsMark.length ;
+
+console.log(studentsTotalMarksArray);
+console.log(totalMarks);
+console.log(avarageMarks);
+
